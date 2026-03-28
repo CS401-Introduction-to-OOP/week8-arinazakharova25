8ab9a29b6f3f82c742bfc6f096246a0066391d5f
arina <arina.zakharova2008@gmail.com>
2026-03-28 14:48:08 +0200

    program class

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Week8;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Letter letter1 = new Letter("12", 0.20);
+        Letter letter2 = new Letter("13", 0.55);
+
+        Parcel parcel1 = new Parcel("17", 12, "30x20x15");
+        Parcel parcel2 = new Parcel("67", 16, "50x40x20");
+    
+        letter1.PrintInfo();
+        parcel1.PrintInfo();
+        Console.WriteLine();
+
+        CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+        myCargo.AddItem(letter1);
+        myCargo.AddItem(letter2);
+        myCargo.AddItem(parcel1);
+        myCargo.AddItem(parcel2);
+        
+        double totalSum = myCargo.GetTotalCost();
+        Console.WriteLine($"Total Shipping Cost: {totalSum} UAH");
+     
+    }
+}
+
